d717044b414d3db15b8994ccb2fb654e0bd34f0a
VitorGadonski <115477096+VitorGadonski@users.noreply.github.com>
2022-11-07 09:48:06 -0300

    Create 2.js

---
+++ 2.js
@@ -0,0 +1,9 @@
+var qtde abacate = parseInt(prompt("quantos abacates?"));
+
+if(qtde_abacate >= 12){
+    valor_total = qtde abacate * 4;
+}else{
+    valor_titak = qtde_abacates * 5;
+}
+
+alert("R$ "+valor total.toFixed(2)); 
